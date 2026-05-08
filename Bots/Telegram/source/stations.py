c3e0309a2c4cfa38b8eae6c5e37cbb05ccb42a37
Rinalon <124454184+Rinalon@users.noreply.github.com>
2026-05-08 22:31:49 +0300

    Update stations.py

--- Bots/Telegram/source/stations.py
+++ Bots/Telegram/source/stations.py
@@ -1,6 +1,6 @@
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram import Bot, Dispatcher
+from aiogram import Dispatcher
 
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
@@ -8,13 +8,13 @@ dp = Dispatcher(storage=storage)
 class WorkStates(StatesGroup):
     start = State()
     ready = State()
+    viewing = State()
 
 class ConnectDevice(StatesGroup):
     wait_device_type = State()
     wait_hub_id = State()
     wait_sensor_id = State()
     wait_location = State()
-    wait_water_threshold = State()
     wait_battery_threshold = State()
     wait_notifications = State()
     wait_shutoff = State()
@@ -31,6 +31,3 @@ class DeleteDevice(StatesGroup):
     waiting_device_type = State()
     waiting_device_id = State()
     waiting_confirm = State()
-
-class AddVK(StatesGroup):
-    wait_vk_id = State()
