d5933ff36fc59a144691efc9ff8502e440f262e7
Rinalon <124454184+Rinalon@users.noreply.github.com>
2026-05-08 22:33:45 +0300

    Update utils.py

--- Bots/Telegram/database/utils.py
+++ Bots/Telegram/database/utils.py
@@ -1,9 +1,10 @@
 import database.requests as rq
 
-async def get_sensor_settings(hub_id, sensor_id) -> str:
-    sensor = await rq.get_sensor(hub_id, sensor_id)
-    if not sensor:
-        raise RuntimeError(f"Датчик #{sensor_id} у хаба #{hub_id} не найден")
+async def get_sensor_settings(hub_id=None, sensor_id=None, sensor=None) -> str:
+    if sensor is None:
+        sensor = await rq.get_sensor(hub_id, sensor_id)
+        if not sensor:
+            raise RuntimeError(f"Датчик #{sensor_id} у хаба #{hub_id} не найден")
 
     notify = ["Только в чате", "Только звук", "И звуковое, и в чате"]
 
