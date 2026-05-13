41252b89ec56823eb485d43685ae94eb3927dccb
Rinalon <124454184+Rinalon@users.noreply.github.com>
2026-05-13 17:22:15 +0300

    Update utils.py

--- Bots/Telegram/database/utils.py
+++ Bots/Telegram/database/utils.py
@@ -7,10 +7,12 @@ async def get_sensor_settings(hub_id=None, sensor_id=None, sensor=None) -> str:
             raise RuntimeError(f"Датчик #{sensor_id} у хаба #{hub_id} не найден")
 
     notify = ["Только в чате", "Только звук", "И звуковое, и в чате"]
+    water_lvl = ["Минимальный", "Средний", "Высокий", "Максимальный"]
 
     sensor_data = f"""Настройки для датчика #<b>{sensor.id}</b>:
     Хаб №<b>{sensor.hub_id}</b>
     Место: <code>{sensor.location}</code>
+    Чувствительность:  <code>{water_lvl[sensor.water_threshold]}</code>
     Низкий заряд начиная с <code>{"50%" if sensor.battery_threshold == 1 or sensor.battery_threshold == True else "20%"}</code>
     Режим уведомлений: <code>{notify[sensor.notifications]}</code>
     Право на перекрытие воды: <code>{"Да" if sensor.shutoff == 1 or sensor.shutoff == True else "Нет"}</code>"""
